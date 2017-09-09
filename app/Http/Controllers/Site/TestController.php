9b064e443f2741a2b0f0ffd97cc42127eb21e70a
Mad Raon <muadauyeu24@gmail.com>
2017-09-09 17:47:39 +0700

    fix

--- app/Http/Controllers/Site/TestController.php
+++ app/Http/Controllers/Site/TestController.php
@@ -284,8 +284,8 @@ class TestController extends Controller
 
     public function changePostStartDate()
     {
-    	$id = 1885;
-    	Post::find($id)->update(['start_date' => '2017-01-11 12:00:00']);
+    	$id = 1886;
+    	Post::find($id)->update(['start_date' => '2016-12-31 12:00:00']);
     	dd('changed');
     }
 
